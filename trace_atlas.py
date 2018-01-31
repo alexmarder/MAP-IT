ccfa6ff6c19e78f2333fb77b3bd614d5e96026fb
romain.fontugne <romain.fontugne@gmail.com>
2018-01-31 07:05:17 +0000

    Accept both RIPE Atlas formats, the one from the measurement page (https://atlas.ripe.net/measurements/) and the one from the archive repository (https://ftp.ripe.net/ripe/atlas/data/)

--- trace_atlas.py
+++ trace_atlas.py
@@ -20,7 +20,8 @@ class TraceReader:
         else:
             self.p = self.p = Popen('cat {}'.format(self.filename), shell=True, stdout=PIPE,
                                     universal_newlines=True)
-        return json.load(self.p.stdout) if self.json else self.p.stdout
+
+        return map(json.loads, self.p.stdout) if self.json else self.p.stdout 
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.p.kill()
@@ -53,20 +54,29 @@ def extract_trace(hops):
                         trace[ttl] = False
     return trace
 
+def process_trace(trace, addresses, adjacencies):
+    if 'result' in trace:
+        for hop in trace['result']:
+            if 'result' in hop:
+                addresses.update(reply['from'] for reply in hop['result'] if 'from' in reply)
+                # there is no status code in atlas. Should check it there is a
+                # loop?
+                # if j['stop_reason'] != 'LOOP':
+        trace = extract_trace(trace['result'])
+        if cycle_free(trace):
+                adjacencies.update((x, y) for x, y in zip(trace, trace[1:]) if x and y)
+
 
 def process_trace_file(filename):
     addresses = set()
     adjacencies = set()
     with TraceReader(filename) as f:
-            for trace in f:
-                if 'result' in trace:
-                    for hop in trace['result']:
-                        if 'result' in hop:
-                            addresses.update(reply['from'] for reply in hop['result'] if 'from' in reply)
-                            # there is no status code in atlas. Should check it there is a
-                            # loop?
-                            # if j['stop_reason'] != 'LOOP':
-                    trace = extract_trace(trace['result'])
-                    if cycle_free(trace):
-                        adjacencies.update((x, y) for x, y in zip(trace, trace[1:]) if x and y)
+        for line in f:
+            if isinstance(line, list):
+                # the file contains a list of traceroutes
+                for trace in line:
+                    process_trace(trace, addresses, adjacencies)
+            else:
+                # the file contains one traceroute per line
+                process_trace(line, addresses, adjacencies)
     return adjacencies, addresses
